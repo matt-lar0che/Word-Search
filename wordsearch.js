5d350807eea73add526dcff9dbcf53090387806d
Matt <mlaroc02@uoguelph.ca>
2025-01-13 14:38:12 -0500

    Added simple click and drag colouring to the grid, as well as debug for showing the array of nodes

--- wordsearch.js
+++ wordsearch.js
@@ -1,3 +1,5 @@
+var isPressed = false;
+var nodeArray;
 async function getData() {
     const url = "wordsearch.php";
     try {
@@ -17,16 +19,40 @@ async function getData() {
 function createBoard(board){
     var mainDiv = document.getElementById("gameGrid");
     mainDiv.innerHTML = "";
+    nodeArray = [];
     mainDiv.style.width = 32 * board.length + "px";
     for (let i = 0; i < board.length; i++){
+        var row = [];
         for (let j = 0; j < board.length; j++){
             let node = document.createElement("div");
             node.setAttribute("class","letter");
             node.innerHTML = board[i][j];
+            node.addEventListener("mouseover", () => {
+              if (isPressed){
+                node.setAttribute("class","letter-clicked");
+              }
+            });
+            node.addEventListener("mousedown", () => {
+              node.setAttribute("class","letter-clicked");
+            });
             mainDiv.appendChild(node);
+            row.push(node);
         }
+      nodeArray.push(row);
     }
+}
+function checkValidGuess(){
+  let nodes = document.querySelectorAll(".letter-clicked");
 }
 
 window.addEventListener("load", getData);
+document.addEventListener("mousedown", () => {
+  isPressed = true;
+});
+document.addEventListener("mouseup", () =>{
+  isPressed = false;
+});
+document.getElementById("checkWord").addEventListener("click", () => {
+  console.log(nodeArray);
+});
   
